0e2a028cdb017412967b4c61a8bb106ae10ed3cc
keco185 <keco185@me.com>
2017-08-26 13:19:50 -0400

    adds subsystems

---
+++ SteamPower/src/org/usfirst/frc/team484/robot/subsystems/Climber.java
@@ -0,0 +1,22 @@
+package org.usfirst.frc.team484.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class Climber extends Subsystem {
+
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+    public void climber (double speed) {
+    	
+    }
+    
+}
+
